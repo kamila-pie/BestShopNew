37bf2320cb161bccda016ad3f0014175cf2b1181
kamila-pie <kamilapietraszuk@gmail.com>
2020-07-15 17:52:26 +0200

    zakończenie etapu projektowania na desktop, konfigiracja pod sass i gulp, wstępny podział partials

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const entryPath = ".";
+
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('sass');
+
+function server(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    cb();
+}
+
+// przykładowe zadanie nowa składnia
+function css(){
+    return gulp.src('./scss/main.scss') //main jako gółwny
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "expanded"
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+function watch(cb) {
+    gulp.watch("./scss/**/*.scss", gulp.series(css));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    cb(); //fukcja zwrotna
+}
+
+module.exports.default = gulp.series(css, server, watch);
+module.exports.watch = watch;
+module.exports.css = css;
